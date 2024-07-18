526c42435be9710416866454deea500b180edcba
NikitaSmirniy <nikitos060207@gmail.com>
2024-07-18 14:57:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,54 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int addNumber = 7;
-            int maximumNumber = 103;
+            const string CommandShowText1 = "1";
+            const string CommandShowText2 = "2";
+            const string CommandRandomNumber = "3";
+            const string CommandConsoleClear = "4";
+            const string CommandExit = "5";
 
-            // Используется цикл for, потому-что так код выглядит более компактным, удобным в чтении и не нужно создавать 
-            // ещё какие либо переменные в полях, как например пришлось бы это делать для работы с циклом while
-            for (int i = 5; i <= maximumNumber; i += addNumber)
+            Random random = new Random();
+            int number;
+
+            string userInput;
+            bool isOpen = true;
+
+            Console.WriteLine("Добро пожаловать в программу!\n");
+
+            while(isOpen)
             {
-                Console.Write(i + "\n");
+                Console.Write($"Команда {CommandShowText1} вывести текст документа {CommandShowText1}\n");
+                Console.Write($"Команда {CommandShowText2} вывести текст документа {CommandShowText2}\n");
+                Console.Write($"Команда {CommandRandomNumber} вывести рандомное число\n");
+                Console.Write($"Команда {CommandConsoleClear} очистить консоль!\n");
+                Console.WriteLine($"Команда {CommandExit} выйти из консоли!");
+
+                userInput = Console.ReadLine();
+                
+                switch(userInput)
+                {
+                    case CommandShowText1:
+                        Console.WriteLine("Документ 1\n\n");
+                            break;
+                    case CommandShowText2:
+                        Console.WriteLine("Докумет 2\n\n");
+                        break;
+                    case CommandRandomNumber:
+                        number = random.Next(0, 101);
+                        Console.WriteLine($"Вы вывели рандомное число и оно равно {number}\n\n");
+                        break;
+                    case CommandConsoleClear:
+                        Console.Clear();
+                        Console.Write("Консоль очищена\n\n");
+                        break;
+                    case CommandExit:
+                        Console.Write("Вы вышли из прогараммы");
+                        isOpen = false;
+                        break;
+                    default:
+                        Console.Write("Такой команды не существует!\n\n");
+                        break;
+                }
             }
 
             Console.ReadLine();
