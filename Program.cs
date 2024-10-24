2d6aad318e71d622aace41583ae223a7791ead7f
NikitaSmirniy <nikitos060207@gmail.com>
2024-10-24 15:52:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,7 +27,13 @@ namespace ConsoleApp1
                     {
                         sum += array[i, j];
                     }
+                }
+            }
 
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
                     if (j == 0)
                     {
                         productOfNumber *= array[i, j];
@@ -35,7 +41,7 @@ namespace ConsoleApp1
 
                     Console.Write(array[i, j] + " ");
                 }
-                
+
                 Console.WriteLine();
             }
 
