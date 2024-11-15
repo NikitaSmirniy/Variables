3f36956e584bedd6a9edac86a84cbad401933c6e
NikitaSmirniy <nikitos060207@gmail.com>
2024-11-15 21:46:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,42 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[30];
+            string text = "You  will die. You good  bro  ";
+            char[] separators = new char[] { ' ', '.' };
 
-            Random random = new Random();
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
-            int maxRandomNumber = 100;
-
-            Console.Write("Не отсортированный массив: ");
-
-            for (int i = 0; i < numbers.Length; i++)
+            foreach (var word in words)
             {
-                numbers[i] = random.Next(0, maxRandomNumber + 1);
-
-                Console.Write(numbers[i] + " ");
-            }
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = 0; j < numbers.Length - 1 - i; j++)
-                {
-                    int temp;
-
-                    if (numbers[j] > numbers[j + 1])
-                    {
-                        temp = numbers[j];
-                        numbers[j] = numbers[j + 1];
-                        numbers[j + 1] = temp;
-                    }
-                }
-            }
-
-            Console.WriteLine("\n");
-            Console.Write("Отсортированный массив: ");
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Write(numbers[i] + " ");
+                Console.WriteLine(word);
             }
 
             Console.ReadLine();
