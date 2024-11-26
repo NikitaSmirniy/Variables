06c02315b372dbe71eee712ae082ff1770a5073d
NikitaSmirniy <nikitos060207@gmail.com>
2024-11-26 15:42:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,41 +6,40 @@ namespace ConsoleApp1
     {   
         static void Main(string[] args)
         {
-            int[] array = new int[36];
+            int[] numbers = new int[8];
 
             Random random = new Random();
             int maxRandomNumber = 9;
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                array[i] = (random.Next(0, maxRandomNumber + 1));
+                numbers[i] = (random.Next(0, maxRandomNumber + 1));
 
-                ShowArray(array, i);
+                ShowArray(numbers, i);
             }
 
             Console.WriteLine();
 
-            Shuffle(array);
+            Shuffle(numbers);
 
             Console.ReadLine();
         }
 
         static void Shuffle(int[] array)
         {
-            int[] tempArray = new int[array.Length];
-            Random random = new Random();
+            var random = new Random();
 
-            for (int i = 0; i < tempArray.Length; i++)
+            for (int i = array.Length - 1; i > 0; i--)
             {
-                int randomIndex = random.Next(0, array.Length);
+                int randomNumber = random.Next(i + 1);
+                int temp = array[randomNumber];
 
-                tempArray[i] = array[randomIndex];
-                RemoveAt(ref array, randomIndex);
-
-                ShowArray(tempArray, i);
+                array[randomNumber] = array[i];
+                array[i] = temp;
             }
 
-            array = tempArray;
+            for (int i = 0; i < array.Length; i++)
+                ShowArray(array, i);
         }
 
         static void ShowArray(int[] array, int indexArray)
@@ -49,19 +48,6 @@ namespace ConsoleApp1
                 Console.Write($"{array[indexArray]}, ");
             else
                 Console.Write($"{array[indexArray]}");
-        }
-
-        static void RemoveAt(ref int[] array, int index)
-        {
-            int[] newArray = new int[array.Length - 1];
-
-            for (int i = 0; i < index; i++)
-                newArray[i] = array[i];
-
-            for (int i = index + 1; i < array.Length; i++)
-                newArray[i - 1] = array[i];
-
-            array = newArray;
         }
     }
 }
