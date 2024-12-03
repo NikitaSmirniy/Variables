64214cc9aa73c8e918d2000346d26f10a0f24fcd
NikitaSmirniy <nikitos060207@gmail.com>
2024-12-03 22:07:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,179 +6,79 @@ namespace ConsoleApp1
     {   
         static void Main(string[] args)
         {
-            const string CommandAddDossier = "1";
-            const string CommandShowAllDossier = "2";
-            const string CommandDeleteDosier = "3";
-            const string CommandSearchByLastName = "4";
-            const string CommandExit = "5";
-
-            string[] names = new string[0];
-            string[] jopPosition = new string[0];
-
-            bool isOpen = true;
-
-            while (isOpen)
+            char[,] map =
             {
-                Console.Clear();
+                {'#', '#', '#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', '#', '#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', '#', '#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', '#', '#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', ' ', '#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', ' ', ' ',' ',' ',' ',' ','#','#','#','#','#','#','#','#'},
+                {'#', '#', ' ',' ','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#', '#', '#',' ','#','#','#','#',' ',' ',' ',' ','#','#','#'},
+                {'#', '#', '#',' ','#','#','#','#',' ','#','#',' ','#','#','#'},
+                {'#', '#', '#',' ','#','#','#','#',' ','#','#',' ','#',' ','#'},
+                {'#', '#', '#',' ','#','#','#',' ',' ',' ','#',' ','#',' ','#'},
+                {'#', '#', '#',' ','#','#','#',' ',' ',' ','#',' ',' ',' ','#'},
+                {'#', '#', '#',' ','#','#','#',' ',' ',' ','#','#','#',' ','#'},
+                {'#', '#', '#',' ',' ',' ',' ',' ',' ','#','#','#','#',' ','#'},
+                {'#', '#', '#','#','#','#','#','#','#','#','#','#','#','#','#'}
+            };
 
-                Console.WriteLine($"Добавить досье - {CommandAddDossier}");
-                Console.WriteLine($"Вывести все досье - {CommandShowAllDossier}");
-                Console.WriteLine($"Удалить досье - {CommandDeleteDosier}");
-                Console.WriteLine($"Поиск по фамилии - {CommandSearchByLastName}");
-                Console.WriteLine($"Выйти из программы - {CommandExit}");
+            ShowMap(map);
 
-                Console.Write("Выберите команду: ");
-                string userInput = Console.ReadLine();
-
-                switch (userInput)
-                {
-                    case CommandAddDossier:
-                        AddDossier(ref names, ref jopPosition);
-                        break;
-
-                    case CommandShowAllDossier:
-                        ShowAllDossier(names, jopPosition, names.Length);
-                        break;
-
-                    case CommandDeleteDosier:
-                        DeleteDossier(ref names, ref jopPosition);
-                        break;
-
-                    case CommandSearchByLastName:
-                        SearchDossier(names, jopPosition);
-                        break;
-
-                    case CommandExit:
-                        isOpen = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Введина неверная команда");
-                        break;
-                }
-
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
 
-        static void ShowDossier(string[] names, string[] jopPosition, int index)
+        static void ShowMap(char[,] map)
         {
-            Console.WriteLine($"{index + 1} - ФИО {names[index]}, Должность {jopPosition[index]}");
-        }
+            int userX = 5;
+            int userY = 1;
+            char obstacle = '#';
+            char player = '@';
 
-        static void ShowAllDossier(string[] names, string[] jopPosition, int elementsLength)
-        {
-            for (int i = 0; i < elementsLength; i++)
-                ShowDossier(names, jopPosition, i);
-        }
+            Console.CursorVisible = false;
 
-        static string[] RemoveAt(string[] array, int index)
-        {
-            string[] newNames = new string[array.Length - 1];
-            index--;
-
-            for (int i = 0; i < index; i++)
-                newNames[i] = array[i];
-
-            for (int i = index + 1; i < array.Length; i++)
-                newNames[i - 1] = array[i];
-
-            return newNames;
-        }
-
-        static void DeleteDossier(ref string[] names, ref string[] jopPosition)
-        {
-            bool isOpen = true;
-
-            while (isOpen && names.Length > 0)
+            while (true)
             {
-                Console.Clear();
+                Console.SetCursorPosition(0, 0);
 
-                ShowAllDossier(names, jopPosition, names.Length);
-
-                Console.Write("Досье под каким номером вы хотите удалить: ");
-                string userInput = Console.ReadLine();
-
-                int shift;
-                bool isSuccess = int.TryParse(userInput, out shift);
-
-                Console.WriteLine(shift);
-
-                if (isSuccess && shift > 0 && shift <= names.Length)
+                for (int i = 0; i < map.GetLength(0); i++)
                 {
-                    names = RemoveAt(names, shift);
-                    jopPosition = RemoveAt(jopPosition, shift);
-
-                    isOpen = false;
-                }
-                else
-                {
-                    Console.WriteLine("Неверная комманда!");
-                    Console.ReadLine();
-                }
-            }
-        }
-
-        static string[] CopyElements(string[] copiedArray, string[] array)
-        {
-            for (int i = 0; i < copiedArray.Length; i++)
-                array[i] = copiedArray[i];
-
-            return array;
-        }
-
-        static void FillElement(string[] filliedArray, string questionText)
-        {
-            Console.Write($"{questionText} - ");
-            string fillText = Console.ReadLine();
-            filliedArray[filliedArray.Length - 1] = fillText;
-        }
-
-        static void AddDossier(ref string[] names, ref string[] jobPosition)
-        {
-            string[] tempNames = new string[names.Length + 1];
-            string[] tempJobPosition = new string[jobPosition.Length + 1];
-
-            names = CopyElements(names, tempNames);
-            jobPosition = CopyElements(jobPosition, tempJobPosition);
-
-            FillElement(names, "Введите ФИО сотрудника");
-            FillElement(jobPosition, "Введите должность сотрудника");
-        }
-
-        static void SearchDossier(string[] names, string[] jopPosition)
-        {
-            const string CommandExit = "Exit";
-
-            bool isOpen = true;
-
-            while (isOpen && names.Length > 0)
-            {
-                Console.Clear();
-
-                Console.Write("Введите Фамилию сотрудника: ");
-                string userInput = Console.ReadLine();
-                bool isEmptySurname = true;
-
-                for (int i = 0; i < names.Length; i++)
-                {
-                    string surname = names[i].Split()[0];
-
-                    if (userInput.ToLower() == surname.ToLower())
+                    for (int j = 0; j < map.GetLength(1); j++)
                     {
-                        ShowDossier(names, jopPosition, i);
-                        isEmptySurname = false;
+                        Console.Write(map[i, j]);
                     }
+
+                    Console.WriteLine();
                 }
 
-                if (isEmptySurname)
-                    Console.WriteLine("Такой Фамилии нет в списках");
+                Console.SetCursorPosition(userY, userX);
+                Console.Write(player);
+                ConsoleKeyInfo charKey = Console.ReadKey();
 
-                Console.Write($"Введите команду - {CommandExit} если хотите прекратить поиск: ");
-                userInput = Console.ReadLine();
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (map[userX - 1, userY] != obstacle)
+                            userX--;
+                        break;
 
-                if (userInput == CommandExit)
-                    isOpen = false;
+                    case ConsoleKey.DownArrow:
+                        if (map[userX + 1, userY] != obstacle)
+                            userX++;
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (map[userX, userY - 1] != obstacle)
+                            userY--;
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (map[userX, userY + 1] != obstacle)
+                            userY++;
+                        break;
+                }
             }
         }
     }
