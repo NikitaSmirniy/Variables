2ac8e622a16ab011ea731f37c3d8d21cca4392be
NikitaSmirniy <nikitos060207@gmail.com>
2025-02-20 21:34:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,54 +6,27 @@ namespace ConsoleApp1
     {   
         static void Main(string[] args)
         {
-            const string RussiaWordCommand = "Россия";
-            const string CanadaWordCommand = "Канада";
-            const string ChinaWordCommand = "Китай";
-            const string USAWordCommand = "США";
-            const string BrazilWordCommand = "Бразилия";
+            List<int> cashSumsClients = new List<int>();
+            int clientsAmount = 10;
+            int cashAccount = 0;
 
-            const string ExitCommand = "выход";
+            Random random = new Random();
+            int minNumber = 5;
+            int maxNumber = 100;
 
-            Dictionary<string, int> explanatoryDictionary = new Dictionary<string, int>();
+            for (int i = 0; i < clientsAmount; i++)
+                cashSumsClients.Add(random.Next(minNumber, maxNumber + 1));
 
-            bool isOpen = true;
-
-            explanatoryDictionary.Add(RussiaWordCommand, 1);
-            explanatoryDictionary.Add(CanadaWordCommand, 2);
-            explanatoryDictionary.Add(ChinaWordCommand, 3);
-            explanatoryDictionary.Add(USAWordCommand, 4);
-            explanatoryDictionary.Add(BrazilWordCommand, 5);
-
-            while (isOpen)
+            for (int i = 0; i < cashSumsClients.Count; i++)
             {
                 Console.Clear();
-                Console.ForegroundColor = ConsoleColor.White;
 
-                Console.WriteLine("Введите любую из этих стран, что-бы узнать на каком месте она по площади в мире");
+                Console.WriteLine($"Клиент №{i + 1} Cумма покупкт клиента: {cashSumsClients[i]}");
+                Console.WriteLine($"Ваш счёт: {cashAccount}");
+                cashAccount += cashSumsClients[i];
+                cashSumsClients.Remove(i);
 
-                foreach (var item in explanatoryDictionary.Keys)
-                {
-                    Console.WriteLine(item);
-                }
-
-                string userInput = Console.ReadLine();
-
-                if (explanatoryDictionary.ContainsKey(userInput))
-                    Console.WriteLine($"{userInput} находится на {explanatoryDictionary[userInput]} месте по площади в мире");
-                else
-                    Console.WriteLine("Такой страны нет в списке");
-                ShowMessage(ConsoleColor.Red, $"Напишите комманду {ExitCommand}, что-бы выйти");
-                
-                userInput = Console.ReadLine();
-
-                if (userInput == ExitCommand)
-                    isOpen = false;
-            }
-
-            void ShowMessage(ConsoleColor color, string message)
-            {
-                Console.ForegroundColor = color;
-                Console.Write(message);
+                Console.ReadLine();
             }
         }
     }
