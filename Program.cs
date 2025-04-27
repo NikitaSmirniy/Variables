4b0686e390bc794cf71170ad5e6bdc42a2f988f6
NikitaSmirniy <nikitos060207@gmail.com>
2025-04-27 21:26:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,396 +7,171 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Arena arena = new Arena();
+            Supermarket supermark = new Supermarket(10);
 
-            arena.Work();
+            supermark.Work();
         }
     }
 
-    class Arena
+    class Supermarket
     {
-        private const string CommandShowFight = "1";
-        private const string CommandExit = "2";
+        private StringDelimiter stringDelimiter = new StringDelimiter(30);
+        private List<Item> _items = new List<Item>();
+        private Queue<Client> _clients = new Queue<Client>();
+        private int _clientsCount;
+        private int _money;
 
-        private StringDelimiter _stringDelimiter = new StringDelimiter(30);
-        private Randomizer _randomizer = new Randomizer();
-
-        private List<Warrior> _warriors;
-
-        public Arena()
+        public Supermarket(int clientsCount)
         {
-            _warriors = new List<Warrior>()
-        {
-            new Necromancer(125, 12, 75,
-            "Это колдун из древнего племени 'индейцев' способен вызывать мертвых которые служат своему хозяину."),
-            new Viking(200, 15, 50, "Этот викинг пришёл на арену, что-бы выплеснуть свою ярость."),
-            new ManTree(400, 0, 25, "Это существо когда-то было человеком, но попав под влияние собственной магии потеряло человеческий облик и обрёл быструю регенирацию."),
-            new Magician(100, 6, 60, "Это маг из древнего племени 'индейцев' способен выпускать огненные шары.", 100),
-            new Archer(175, 25, 65, "Этот лучник имеет большой опыт в боях, на арену он пришёл исключительно за наживой.")
-        };
+            _clientsCount = clientsCount;
+
+            AddItems();
+
+            AddClients();
         }
 
         public void Work()
         {
-            bool isOpen = true;
+            ShowItems();
 
-            Console.WriteLine("Приветствую вас на арене гладиаторских боёв");
+            stringDelimiter.DrawLine();
 
-            while (isOpen)
-            {
-                Console.WriteLine($"Команда - {CommandShowFight} посмотреть бой");
-                Console.WriteLine($"Команда - {CommandExit} выйти");
+            ServeClients();
 
-                Console.Write("Выберете комнаду: ");
+            stringDelimiter.DrawLine();
 
-                string userInput = Console.ReadLine();
-
-                switch (userInput)
-                {
-                    case CommandShowFight:
-                        StartFight();
-                        break;
-
-                    case CommandExit:
-                        isOpen = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Вы ввели неверную команду");
-                        break;
-                }
-
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-
-        private void StartFight()
-        {
-            ShowWarriors();
-
-            if (TryGetWarrior(out Warrior warrior1) == false || TryGetWarrior(out Warrior warrior2) == false)
-                return;
-
-            Console.Clear();
-
-            while (warrior1.Health > 0 && warrior2.Health > 0)
-            {
-                warrior1.Attack(warrior2);
-                warrior2.ShowCurrentHealth();
-
-                _stringDelimiter.DrawLine();
-
-                warrior2.Attack(warrior1);
-                warrior1.ShowCurrentHealth();
-
-                Console.ReadKey();
-                Console.Clear();
-            }
-
-            ShowBattleOutcome(warrior1, warrior2);
-        }
-
-        private bool TryGetWarrior(out Warrior warrior)
-        {
-            Console.Write("Выберите гладиатора: ");
-
-            if (int.TryParse(Console.ReadLine(), out int heroIndex))
-            {
-                if (heroIndex - 1 >= 0 && heroIndex - 1 < _warriors.Count)
-                {
-                    Warrior newWarrior = _warriors[heroIndex - 1];
-                    warrior = newWarrior.Clone();
-
-                    Console.WriteLine($"Вы выбрали гладиатора под номером - {heroIndex}");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Такого бойца нет в списке!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Вы ввели неверные символы!");
-            }
-
+            Console.WriteLine($"Работа завершена\n за сегодня мы заработали: {_money} долларс");
             Console.ReadKey();
-            warrior = null;
-            return false;
         }
 
-        private void ShowWarriors()
+        private void AddClients()
         {
-            Console.WriteLine("**Список гладиаторов**");
+            Random random = new Random();
+            int minRandomMoney = 1000;
+            int maxRandomMoney = 2500;
 
-            for (int i = 0; i < _warriors.Count; i++)
+            for (int i = 0; i < _clientsCount; i++)
+                _clients.Enqueue(new Client(FillClientCart(), random.Next(minRandomMoney, maxRandomMoney)));
+        }
+
+        private void AddItems()
+        {
+            _items.Add(new Item("Яблоко 'Забродившее'", 50));
+            _items.Add(new Item("Водка 'Тундра'", 300));
+            _items.Add(new Item("Чупс со вкусом пива", 10));
+            _items.Add(new Item("Чипсы 'Пивные'", 400));
+            _items.Add(new Item("Вино", 500));
+            _items.Add(new Item("Пиво 'Гиннесс'", 200));
+        }
+
+        private void ServeClients()
+        {
+            while (_clients.Count > 0)
             {
-                _stringDelimiter.DrawLine();
+                _money += _clients.Dequeue().Buy();
 
-                Console.Write($"Гладиатор №{i + 1}. ");
-                _warriors[i].ShowStats();
+                stringDelimiter.DrawLine();
+
+                Console.WriteLine("Клиент обслужен");
+                Console.ReadKey();
+
+                stringDelimiter.DrawLine();
             }
         }
 
-        private void ShowBattleOutcome(Warrior warrior1, Warrior warrior2)
+        private List<Item> FillClientCart()
         {
-            if (warrior1.Health <= 0 && warrior2.Health <= 0)
-                Console.WriteLine("В этом бою победила ничья");
-            else if (warrior1.Health <= 0)
-                Console.WriteLine("В этом бою победил второй гладиатор");
-            else if (warrior2.Health <= 0)
-                Console.WriteLine("В этом бою победил первый гладиатор");
-        }
-    }
+            List<Item> items = new List<Item>();
 
-    enum WarriorType { Magician, Archer, Viking, Necromancer, ManTree }
+            int minItemCount = 1;
+            int maxItemCount = 10;
+            int randomItemsCount = Randomizer.GenerateRandomValue(minItemCount, maxItemCount);
 
-    abstract class Warrior
-    {
-        protected int MaxHealth;
-        protected int Armor;
-        protected int Damage;
-        protected string Description;
+            for (int i = 0; i < randomItemsCount; i++)
+                items.Add(_items[Randomizer.GenerateRandomValue(0, _items.Count - 1)].Clone());
 
-        public Warrior(int maxHealth, int armor, int damage, string description, WarriorType type)
-        {
-            MaxHealth = GetStat(maxHealth);
-            Armor = GetStat(armor);
-            Damage = GetStat(damage);
-
-            Health = MaxHealth;
-            Description = description;
-            Type = type;
+            return items;
         }
 
-        public int Health { get; protected set; }
-
-        public WarriorType Type { get; private set; }
-
-        public virtual void TakeDamage(int damage)
+        private void ShowItems()
         {
-            if (damage < Armor)
-                Health -= damage - Armor;
-            else
-                Health--;
-        }
+            Console.WriteLine("Ассортимент товаров");
 
-        public virtual void Attack(Warrior enemy)
-        {
-            if (Health > 0)
-                enemy.TakeDamage(Damage);
-        }
-
-        public void ShowStats()
-        {
-            Console.WriteLine($"\nХарактеристики\nТип: {Type}\nЗдоровье: {Health}\nБроня: {Armor}\nУрон: {Damage}");
-            Console.WriteLine($"Описание: {Description}");
-        }
-
-        public void ShowCurrentHealth()
-        {
-            Console.WriteLine($"{Type } Здоровье: {Health}/{MaxHealth}");
-        }
-
-        public abstract Warrior Clone();
-
-        private int GetStat(int stat)
-        {
-            if (stat > 0)
-                return stat;
-
-            return 1;
-        }
-    }
-
-    class Archer : Warrior
-    {
-        private int _dodgeChance = 50;
-
-        private Randomizer _randomizer = new Randomizer();
-
-        public Archer(int maxHealth, int armor, int damage, string description) :
-            base(maxHealth, armor, damage, description, WarriorType.Archer)
-        { }
-
-        public override void TakeDamage(int damage)
-        {
-            if (_randomizer.GenerateRndomValue() >= _dodgeChance)
-                base.TakeDamage(damage);
-            else
-                Console.WriteLine($"{Type} задоджил атаку");
-        }
-
-        public override Warrior Clone()
-        {
-            return new Archer(Health, Armor, Damage, Description);
-        }
-    }
-
-    class Necromancer : Warrior
-    {
-        private int _doubleDamageChance = 50;
-        private int _damageModify = 2;
-
-        private Randomizer _randomizer = new Randomizer();
-
-        public Necromancer(int maxHealth, int armor, int damage, string description) :
-            base(maxHealth, armor, damage, description, WarriorType.Necromancer)
-        { }
-
-        public override void Attack(Warrior enemy)
-        {
-            if (_randomizer.GenerateRndomValue() > _doubleDamageChance)
-            {
-                enemy.TakeDamage(Damage * _damageModify);
-                Console.WriteLine($"{Type} Наносит крит. урон");
-            }
-            else
-            {
-                enemy.TakeDamage(Damage);
-            }
-        }
-
-        public override Warrior Clone()
-        {
-            return new Necromancer(Health, Armor, Damage, Description);
-        }
-    }
-
-    class Magician : Warrior
-    {
-        private int _mana;
-        private int _maxMana;
-        private int _abilutyPrice = 25;
-        private int _damageModify = 2;
-
-        public Magician(int maxHealth, int armor, int damage, string description, int maxMana) :
-            base(maxHealth, armor, damage, description, WarriorType.Magician)
-        {
-            _maxMana = maxMana;
-            _mana = _maxMana;
-        }
-
-        public override void Attack(Warrior enemy)
-        {
-            if (_mana >= _abilutyPrice)
-            {
-                _mana -= _abilutyPrice;
-
-                enemy.TakeDamage(Damage * _damageModify);
-
-                Console.WriteLine($"{Type} Нанёс урон огненным шаром");
-            }
-            else
-            {
-                enemy.TakeDamage(Damage);
-            }
-        }
-
-        public override Warrior Clone()
-        {
-            return new Magician(Health, Armor, Damage, Description, _maxMana);
-        }
-    }
-
-    class Viking : Warrior
-    {
-        private int _rage;
-        private int _limitRage = 3;
-
-        private int _rageAttackCount = 2;
-
-        public Viking(int maxHealth, int armor, int damage, string description) :
-            base(maxHealth, armor, damage, description, WarriorType.Viking)
-        { }
-
-        public override void Attack(Warrior enemy)
-        {
-            _rage++;
-
-            if (_rage >= _limitRage)
-            {
-                for (int i = 0; i < _rageAttackCount; i++)
-                {
-                    enemy.TakeDamage(Damage);
-                }
-
-                _rage = 0;
-
-                Console.WriteLine($"{Type} Впал в ярость и нанес сразу несколько ударов по сопернику");
-            }
-            else
-            {
-                enemy.TakeDamage(Damage);
-            }
-        }
-
-        public override Warrior Clone()
-        {
-            return new Viking(Health, Armor, Damage, Description);
-        }
-    }
-
-    class ManTree : Warrior
-    {
-        private int _rage;
-        private int _limitRage = 4;
-        private int _healthShare = 2;
-
-        public ManTree(int maxHealth, int armor, int damage, string description) :
-            base(maxHealth, armor, damage, description, WarriorType.ManTree)
-        { }
-
-        private int HealRepair => MaxHealth / _healthShare;
-
-        public override void TakeDamage(int damage)
-        {
-            base.TakeDamage(damage);
-            _rage++;
-
-            CureHealth();
-        }
-
-        public override Warrior Clone()
-        {
-            return new ManTree(Health, Armor, Damage, Description);
-        }
-
-        private void CureHealth()
-        {
-            if (_rage >= _limitRage && Health < MaxHealth)
-            {
-                int necessaryHealth = MaxHealth - Health;
-
-                if (HealRepair <= necessaryHealth)
-                    Health += HealRepair;
-                else
-                    Health += necessaryHealth;
-
-                _rage = 0;
-                Console.WriteLine($"{Type} накопил ярость и вылечил себе часть здоровья");
-            }
+            foreach (var item in _items)
+                item.ShowInfo();
         }
     }
 
     class Client
     {
-        private List<Item> _products = new List<Item>();
-        private List<Item> _bag = new List<Item>();
+        private List<Item> _cartItems = new List<Item>();
+        private List<Item> _bagItems;
 
         public Client(List<Item> products, int money)
         {
-            _products = products;
+            _cartItems = products;
             Money = money;
         }
 
         public int Money { get; private set; }
 
-        //public bool TryBuy(int )
-        //{
-        //    if
-        //}
+        public int Buy()
+        {
+            while (_cartItems.Count > 0)
+            {
+                if (Money >= GetSumBuy(out int sumBuy))
+                {
+                    Console.WriteLine("Я купил продукты");
+                    Console.ReadKey();
+
+                    _bagItems = new List<Item>(_cartItems);
+                    _cartItems.Clear();
+                    ShowBag();
+
+                    return sumBuy;
+                }
+                else
+                {
+                    Item dropItem = GetRandomItemFromCart();
+
+                    Console.WriteLine("Мне не хватает денег для покупки и мне пришлось убрать один продукт из корзины ");
+                    dropItem.ShowInfo();
+
+
+                    _cartItems.Remove(dropItem);
+
+                    Console.ReadKey();
+                }
+            }
+
+            Console.WriteLine("Я не смог ничего купить");
+            Console.ReadKey();
+
+            return 0;
+        }
+
+        public int GetSumBuy(out int sumBuy)
+        {
+            sumBuy = 0;
+
+            foreach (var item in _cartItems)
+                sumBuy += item.Price;
+
+            return sumBuy;
+        }
+
+        private Item GetRandomItemFromCart()
+        {
+            return _cartItems[Randomizer.GenerateRandomValue(0, _cartItems.Count - 1)];
+        }
+
+        private void ShowBag()
+        {
+            Console.WriteLine("**Вот список мои купленных продуктов**");
+
+            foreach (var bagItem in _bagItems)
+                bagItem.ShowInfo();
+        }
     }
 
     class Item
@@ -409,6 +184,16 @@ namespace ConsoleApp1
 
         public string Name { get; private set; }
         public int Price { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} цена: {Price}");
+        }
+
+        public Item Clone()
+        {
+            return new Item(Name, Price);
+        }
     }
 
     class StringDelimiter
@@ -422,18 +207,17 @@ namespace ConsoleApp1
 
         public void DrawLine()
         {
-            Console.WriteLine(new string('_', _lineRange));
+            Console.WriteLine(new string('-', _lineRange));
         }
     }
 
-    class Randomizer
+    static class Randomizer
     {
-        private Random _random = new Random();
-        private int _maxRandomValue = 100;
+        private static Random _random = new Random();
 
-        public int GenerateRndomValue()
+        public static int GenerateRandomValue(int minRandomValue, int maxRandomValue)
         {
-            return _random.Next(0, _maxRandomValue + 1);
+            return _random.Next(minRandomValue, maxRandomValue + 1);
         }
     }
 }
